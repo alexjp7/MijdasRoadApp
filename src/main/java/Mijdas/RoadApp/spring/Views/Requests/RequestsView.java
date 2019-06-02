d2ca8333186da28b750bd275021e429f45ce2fde
Mitch <Mitch@MITCH-PC.gateway>
2019-06-02 17:23:42 +1000

    back end messaging saving/sending added
    -need to add UI in requestview for initial msg

--- src/main/java/Mijdas/RoadApp/spring/Views/Requests/RequestsView.java
+++ src/main/java/Mijdas/RoadApp/spring/Views/Requests/RequestsView.java
@@ -11,9 +11,13 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.Column;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.PopupView;
 
 
 @Route(value = "requests", layout = MainLayout.class)
@@ -37,14 +41,33 @@ public class RequestsView extends Div
        grid.setHeightFull();
        grid.setColumns("motoristUsername","nearestAddress","details");
        
+//       grid.setSelectionMode(Grid.SelectionMode.NONE);
+//       grid.setItemDetailsRenderer(TemplateRenderer.<Requests> of(
+//               "<div class='custom-details' style='border: 1px solid gray; padding: 10px; width: 100%; box-sizing: border-box;'>"
+//                       +"<div>Hi Test!</div>"
+//                       +"</div>")
+//               .withEventHandler("handleClick", c -> {
+//                   grid.getDataProvider().refreshItem(c);
+//                   System.out.println("TESTTTTT");
+//               }));
+//       grid.setDetailsVisibleOnClick(false);
        
+//       
+//       VerticalLayout popupContent = new VerticalLayout(); 
+//       popupContent.addComponentAsFirst(new TextField("TextField"));
+//       popupContent.addComponentAtIndex(1, new Button("Send"));
+//       PopupView popup = new PopupView("", (Component) popupContent);
        
+
        add(grid);
        updateList();
        
        //This line below breaks the view:
        grid.addComponentColumn(car -> new Label(regoController.getRego(profileController.getUser(car.getMotoristUsername()).getLicenseNum().toString()).getModel())).setHeader("Car Model");
-       grid.addComponentColumn(button -> new Button("Message",clickEvent ->{System.out.println("Message");})).setHeader("Actions");
+       grid.addComponentColumn(button -> new Button("Message",clickEvent ->{
+           System.out.println("Message Clicked.");
+//           grid.setDetailsVisible(button, !grid.isDetailsVisible(button));
+       })).setHeader("Actions");
        
        
     }
