0f1a42f38bda1c7fabf65598517d24592a494c06
Dylan Du-Bois <dylan.dubois@gmail.com>
2019-06-02 14:11:22 +1000

    Added link to view requests for motorist.

--- src/main/java/Mijdas/RoadApp/spring/Views/Navbar.java
+++ src/main/java/Mijdas/RoadApp/spring/Views/Navbar.java
@@ -66,6 +66,8 @@ public class Navbar extends Div
                 links.get(3).add(new Icon(VaadinIcon.COGS), new Text("Profile"));
                 //Service request page
                 links.get(2).add(new Icon(VaadinIcon.COGS), new Text("Service"));
+                //View motorist requests
+                links.get(4).add(new Icon(VaadinIcon.COGS), new Text("View Requests"));
             }
             else if(SessionController.getInstance().getUserType() == UserType.MECHANIC)
             {
