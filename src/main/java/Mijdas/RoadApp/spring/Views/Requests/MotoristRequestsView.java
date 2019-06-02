0f31e38860b54cc20d5647fe6ad05e47aaf19c9b
Dylan Du-Bois <dylan.dubois@gmail.com>
2019-06-02 19:03:01 +1000

    Added view for motorists to view service requests. Also added a 'Cancel' button that removes the request from the view and database.

--- src/main/java/Mijdas/RoadApp/spring/Views/Requests/MotoristRequestsView.java
+++ src/main/java/Mijdas/RoadApp/spring/Views/Requests/MotoristRequestsView.java
@@ -1,5 +1,6 @@
 package Mijdas.RoadApp.spring.Views.Requests;
 
+import Mijdas.RoadApp.spring.Controllers.ProfileController;
 import Mijdas.RoadApp.spring.Controllers.RegoController;
 import Mijdas.RoadApp.spring.Models.RequestModels.RequestService;
 import Mijdas.RoadApp.spring.Models.RequestModels.Requests;
@@ -11,35 +12,38 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 @Route(value = "requestsMotorist", layout = MainLayout.class)
 @PageTitle("Requests")
 
 public class MotoristRequestsView extends Div {
     private RegoController regoController = new RegoController();
+    private ProfileController profileController = new ProfileController();
     private RequestService service = RequestService.getInstance();
     Grid<Requests> grid = new Grid<>(Requests.class);
     TextField searchField = new TextField();
     public MotoristRequestsView()
     {
-       Button searchButton = new Button("Search", clickEvent->{updateList(searchField.getValue());});
-       searchField.setPlaceholder("Street Name, City, or Postcode..");
-       searchField.setWidth("260px"); //fit in placeholder text
-       add(searchField);
-       add(searchButton);
-       setSizeFull();
-       grid.setHeightFull();
-       grid.setColumns("nearestAddress","details");
+        Button searchButton = new Button("Search", clickEvent->{updateList(searchField.getValue());});
+        searchField.setPlaceholder("Street Name, City, or Postcode..");
+        searchField.setWidth("260px"); //fit in placeholder text
+        add(searchField);
+        add(searchButton);
+        setSizeFull();
+        grid.setHeightFull();
+        grid.setColumns("nearestAddress","details");
        
        
        
-       add(grid);
-       updateList();
+        add(grid);
+        updateList();
+        addActionColumn();
+        //This line below breaks the view:
+
+        //grid.addComponentColumn(button -> new Button("Cancel",clickEvent ->{System.out.println("Cancel");}));
+
        
-       //This line below breaks the view:
-       grid.addComponentColumn(car -> new Label(regoController.getRego("5225").getModel())).setHeader("Car Model");
-//       grid.addComponentColumn(button -> new Button("Message",clickEvent ->{System.out.println("Message");})).setHeader("Actions");
-        grid.addComponentColumn(button -> new Button("Message",clickEvent ->{System.out.println("Message");})).setHeader("Actions");
     }
     
     private void updateList(){
@@ -47,6 +51,29 @@ public class MotoristRequestsView extends Div {
     }
     private void updateList(String s){
         grid.setItems(service.findAllPostCode(s));
+    }
+    
+    private void addActionColumn() {
+        grid.addComponentColumn(car -> new Label(regoController.getRego(profileController.getUser(car.getMotoristUsername()).getLicenseNum().toString()).getModel())).setHeader("Car Model");
+        grid.addComponentColumn(request -> {
+            Button message = new Button("Message",clickEvent ->{
+                
+            });
+            Button cancel = new Button("Cancel");
+            System.out.print("Req #: " + request.getRequestNum());
+            String requestNum = Integer.toString(request.getRequestNum());
+            System.out.println(" Req #: " + requestNum);
+            System.out.println("Request to be deleted: " + requestNum);
+            cancel.addClickListener( clickEvent ->{
+                service.deleteData(requestNum);
+                updateList();
+            });
+            VerticalLayout layout = new VerticalLayout();
+            message.setWidth("100%");
+            cancel.setWidth("100%");
+            layout.add(message, cancel);
+            return layout;
+        }).setHeader("Actions");
     }
 }
 
