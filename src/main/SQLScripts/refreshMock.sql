549eb4ef45c8901536220f2677d87517f70d49b7
Joel Dunstan <48910679+joeld96@users.noreply.github.com>
2019-06-04 23:28:33 +1000

    removing swears

--- src/main/SQLScripts/refreshMock.sql
+++ src/main/SQLScripts/refreshMock.sql
@@ -4,5 +4,7 @@ source dbcreate.sql
 source ./test data/userTest.sql
 source ./test data/motorists.sql
 source ./test data/inserts.sql
+source ./test data/vehicle.sql
+source ./test data/mechanics.sql
 source procedures.sql
 source messages.sql
